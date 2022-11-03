6b305e2467745b8fff481bfbfed655d71266cba5
Geordanis Mondeja <gmondeja32@gmail.com>
2022-11-02 20:34:26 -0700

    changes the structure of the js n html will work on css as well

--- app.js
+++ app.js
@@ -1,81 +1,85 @@
-const questions = [
+const questions = [ //this object i will use to pick out the questions and answers from
     {
-      question: 'What is 2 + 2?',
+      question: ' Which of the following is an advantage of using JavaScript?',
       answers: [
-        { text: '4', correct: true },
-        { text: '22', correct: false }
+        { text: 'Less server interaction', correct: false },
+        { text: 'Immediate feedback to the visitors', correct: false },
+        { text: ' Increased interactivity', correct: false },
+        { text: ' All of the above.', correct: true },
       ]
     },
     {
-      question: 'Who is the best YouTuber?',
+      question: 'Which of the following is the correct syntax to print a page using JavaScript? ',
       answers: [
-        { text: 'Web Dev Simplified', correct: true },
-        { text: 'Traversy Media', correct: true },
-        { text: 'Dev Ed', correct: true },
-        { text: 'Fun Fun Function', correct: true }
+        { text: 'window.print();', correct: true },
+        { text: ' browser.print();', correct: false },
+        { text: 'navigator.print();', correct: false },
+        { text: 'document.print();', correct: false }
       ]
     },
     {
-      question: 'Is web development fun?',
+      question: 'Which built-in method reverses the order of the elements of an array?',
       answers: [
-        { text: 'Kinda', correct: false },
-        { text: 'YES!!!', correct: true },
-        { text: 'Um no', correct: false },
-        { text: 'IDK', correct: false }
+        { text: 'changeOrder(order)', correct: false },
+        { text: 'reverse()', correct: true },
+        { text: 'sort(order)', correct: false },
+        { text: 'None of the about', correct: false }
       ]
     },
     {
-      question: 'What is 4 * 2?',
+      question: 'The != and !== symbols both work the same for inequality comparisons:',
       answers: [
-        { text: '6', correct: false },
-        { text: '8', correct: true }
+        { text: 'True', correct: false },
+        { text: 'False', correct: true }
       ]
-    }
+    },
+    {
+        question:'The expression 8 >= 8 evaluates to',
+        answer: [
+            {text: 'True', correct: true },
+          {  text: 'True', correct: false },
+        ]
+    },
+    {
+        question:'For strict equality comparisons, we should use:',
+        answer: [
+            {text: '==', correct: false },
+          {  text: '===', correct: true },
+        ]
+    },
 ]
-const quiz= document.getElementById('quiz');
-const answerEls = document.querySelectorAll('.answer');
-const questionEl = document.getElementById('question');
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
-const submitBtn = document.getElementById('submit');
-const againBtn = document.getElementById('play-A');
 
-    let shuffledQuestions, currentQuestionIndex
+//created variable of  everything that i will need 
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-    submitBtn.addEventListener('click', startGame)
-    play-A.addEventListener('click', () => {
-      currentQuestionIndex++
-      setNextQuestion()
-    })
+let shuffledQuestions, currentQuestionIndex
 
-
-
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
 function startGame() {
-    submitBtn.classList.add('hide')
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-   
-    setNextQuestion()
-  }
-      function setNextQuestion() {
-        resetState()
-        showQuestion(shuffledQuestions[currentQuestionIndex])
-      }
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
 
-      function showQuestion(question) {
-        questionElement.innerText = questions.question
-        question.answers.forEach(answer => {
-          const button = document.createElement('button')
-          button.innerText = answer.text
-          button.classList.add('btn')
-          if (answer.correct) {
-            button.dataset.correct = answers.correct
-          }
-          button.addEventListener('click', selectAnswer)
-          answerButtonsElement.appendChild(button)
-        })
-      }
-      
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button
