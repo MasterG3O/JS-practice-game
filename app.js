615a09cebb951776e436a704e49c269b74510211
Geordanis Mondeja <gmondeja32@gmail.com>
2022-11-05 22:31:26 -0700

    almost done will style next

--- app.js
+++ app.js
@@ -23,63 +23,164 @@ const questions = [ //this object i will use to pick out the questions and answe
         { text: 'changeOrder(order)', correct: false },
         { text: 'reverse()', correct: true },
         { text: 'sort(order)', correct: false },
-        { text: 'None of the about', correct: false }
+        { text: 'None of the about', correct: false },
       ]
     },
     {
       question: 'The != and !== symbols both work the same for inequality comparisons:',
       answers: [
         { text: 'True', correct: false },
-        { text: 'False', correct: true }
+        { text: 'False', correct: true },
       ]
     },
     {
         question:'The expression 8 >= 8 evaluates to',
-        answer: [
+        answers: [
             {text: 'True', correct: true },
-          {  text: 'True', correct: false },
+          {text: 'True', correct: false },
         ]
     },
     {
-        question:'For strict equality comparisons, we should use:',
-        answer: [
-            {text: '==', correct: false },
-          {  text: '===', correct: true },
+        question:'How to write an IF statement in JavaScript?',
+        answers: [
+          {text: 'if (i == 5)  ', correct: true },
+          {text: 'if i == 5 then', correct: false },
+          {text: 'if i = 5 then', correct: false },
+          {text: 'if i=5', correct: false },
         ]
     },
+    {
+      question:'How does a WHILE loop start?',
+      answers: [
+        {text: 'while (i <= 10)  ', correct: true },
+        {text: 'while (i <= 10; i++)', correct: false },
+        {text: 'while i = 1 to 10', correct: false },
+      ]
+  },
+  {
+    question:'How can you add a comment in a JavaScript?',
+    answers: [
+      {text: '//This is a comment ', correct: true },
+      {text: "'This is a comment'", correct: false },
+      {text: '<!--This is a comment-->', correct: false },
+     
+    ]
+},
+{
+  question:'JavaScript is the same as Java.',
+  answers: [
+    {text: 'True ', correct: false },
+    {text: "False", correct: true },
+    {text: '<!--This is a comment-->', correct: false },
+   
+  ]
+},
+{
+  question:'Which event occurs when the user clicks on an HTML element?',
+  answers: [
+    {text: 'onmouseclick ', correct: false },
+    {text: 'onmouseover', correct: false },
+    {text: 'onclick  ', correct: true },
+    {text: 'oonchange', correct: false },
+  ]
+},
 ]
 
-//created variable of  everything that i will need 
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
 
-let shuffledQuestions, currentQuestionIndex
+//created variable of  everything that i will need buttons
+let startButton = document.getElementById('start-btn')
+let nextButton = document.getElementById('next-btn')
+
+let questionContainerElement = document.getElementById('question-container')
+let questionElement = document.getElementById('question')
+let answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex 
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++
   setNextQuestion()
+  console.log(setNextQuestion)
 })
 
-function startGame() {
+function startGame() { //at the start of the game one the start button is clicked it will hide and 
   startButton.classList.add('hide')
   shuffledQuestions = questions.sort(() => Math.random() - .5)
+// console.log(shuffledQuestions)
   currentQuestionIndex = 0
+ 
   questionContainerElement.classList.remove('hide')
   setNextQuestion()
 }
+function showQuestion(question) { //function shows question and hidens the next button if nothing is answered
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => { // getting an error here not to sure why
+    const button = document.createElement('button')
+  console.log(button)
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+function setNextQuestion() { 
+  reset()
 
-function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
+  showQuestion(shuffledQuestions[currentQuestionIndex]) //"cannot read properties of undefines(reading "for
+}
+// function to show questions upon 
+
+function reset() { 
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
 }
 
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+ 
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+   
+   
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) { //hide button 
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  
+  
+  clearStatusClass(element)
+ 
+  if (correct) {
+    element.classList.add('correct')
+  }  
+  else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+// 1 start game with start button
+// 2 hide the button
+// 3 show next question
+// 4hide the next button until answered is shown
+// 5 once next is clicked go to the next question 
+// 6same thing until the end where it
